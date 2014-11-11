6ab78828efe9403e9cabfe8f05af0317eac61691
treacle74 <ptrelease@gmail.com>
2014-11-11 15:59:54 +0000

    added initial screens and formatting

---
+++ frontend.py
@@ -0,0 +1,21 @@
+__author__ = 'paultrelease'
+
+from flask import Flask, render_template, request
+import os
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template("index.html")
+
+@app.route('/removerestriction', methods=['POST'])
+def remove_restriction():
+    referencenumber = request.form.get('referencenumber')
+    #When this is done properly, a call to another microservice will take place
+    #and if the case is successful then the page below will be rendered.
+    return render_template("restriction_removed.html", referencenumber=referencenumber)
+
+
+
+if __name__ == '__main__':
+    app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 5010)))
