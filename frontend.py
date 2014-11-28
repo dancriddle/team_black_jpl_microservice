c40e954c5d1a782532254af91991a7f790877d3f
srallis1 <srallis@users.noreply.github.com>
2014-11-28 15:14:03 +0000

    Added check title template

--- frontend.py
+++ frontend.py
@@ -9,10 +9,23 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/checkfortitle')
+@app.route('/checkfortitle', methods=['POST'])
 def check_for_title():
     #here, we'll make the call to url = 'http://0.0.0.0:5010/titlefromreference' etc'
-    return render_template("a_new_is_this_your)_title_template.html")
+    url = 'https://team-black-casework-2-srallis1-2.c9.io/titlefromreference'
+    referencenumber = request.form.get('referencenumber')
+    data = {"reference": referencenumber}
+    headers = {'Content-Type': 'application/json'}
+    r = requests.post(url, data=json.dumps(data), headers=headers)
+    titlenumber = r.text
+    
+    app.logger.debug(r.text)
+    
+    if titlenumber == "Referance not valid":
+        return render_template("reference_invalid.html", referencenumber=referencenumber)
+    else:
+        return render_template("reference_valid.html", referencenumber=referencenumber, titlenumber=titlenumber)
+    
     #the above template will call the removerestriction route below.
 
 
@@ -20,17 +33,25 @@ def check_for_title():
 @app.route('/removerestriction', methods=['POST'])
 def remove_restriction():
     referencenumber = request.form.get('referencenumber')
+    titlenumber = request.form.get('titlenumber')
     # Need to find a way to change this to the cloud 9 url when appropriate.
     # Probably set up an environment.sh, that just gets picked up locally.
-    url = 'http://0.0.0.0:5010/titlefromreference'
-    data = {"reference": referencenumber}
-    headers = {'Content-Type': 'application/json'}
-    r = requests.post(url, data=json.dumps(data), headers=headers)
-    titlenumber = r.text
-    app.logger.debug(r.text)
+    #url = 'https://team-black-casework-2-srallis1-2.c9.io/titlefromreference'
+    #data = {"reference": referencenumber}
+    #headers = {'Content-Type': 'application/json'}
+    #r = requests.post(url, data=json.dumps(data), headers=headers)
+    #titlenumber = r.text
+    
+    #app.logger.debug(r.text)
+    
+    #if titlenumber == "Referance not valid":
+        #return render_template("reference_invalid.html", referencenumber=referencenumber)
+    #else:
     return render_template("restriction_removed.html", referencenumber=referencenumber, titlenumber=titlenumber)
+    
+    #return render_template("restriction_removed.html", referencenumber=referencenumber, titlenumber=titlenumber)
     #and here call the casework servivce to remove te restriction flag.
-
+    
 
 if __name__ == '__main__':
     app.run(debug=True,host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 5009)))
